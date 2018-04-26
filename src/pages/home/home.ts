71435cb5cd386f4be285cb82faa4ff4ee79c6ee0
Santyid <santiago-2108@hotmail.com>
2018-04-25 21:33:12 -0500

    add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	constructor(public toastCtrl: ToastController) {}
+	status: string = 'Inicio';
 
-  constructor(public navCtrl: NavController) {
+	showAlert() {
+		const toast = this.toastCtrl.create({
+			message: 'Nice, i like this',
+			showCloseButton: true,
+			closeButtonText: 'Ok'
+		});
+		toast.present();
+	}
 
-  }
-
+	showAlert1() {
+		const toast = this.toastCtrl.create({
+			message: 'Is the moment.',
+			showCloseButton: true,
+			closeButtonText: 'Ok'
+		});
+		toast.present();
+	}
 }
